fd3a620c5bceebaa69be67b293e68d2f6a89597b
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2025-08-01 08:36:20 -0300

    feat: integrate Tailwind CSS and create header component
    - Added Tailwind CSS dependencies to package.json.
    - Updated app.component.html to include a new header component.
    - Created header.component.ts and header.component.html for the header.
    - Modified app.component.ts to import the new header component.
    - Changed index.html language to Portuguese and updated the title.
    - Added global styles import for Tailwind CSS in styles.css.
    - Created a .postcssrc.json configuration file for PostCSS.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    imports: [HeaderComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular-easy-task';
+    title = 'angular-easy-task';
 }
